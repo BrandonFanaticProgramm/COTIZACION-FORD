ebe2acd78dcfa221d2efac7d6f17337b3f749591
BrandonFanaticProgramm <brandonalexisquintero.sena@gmail.com>
2024-12-11 16:53:10 -0500

    Cambiando el puerto del servidor

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const { path } = require("pdfkit");
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 10000;
 app.use(cors());
 app.use(express.json());
 let id_usuario = "";
