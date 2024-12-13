ff477e4ece341d4d0d8f1e4f2d1c68691231b8eb
BrandonFanaticProgramm <brandonalexisquintero.sena@gmail.com>
2024-12-13 18:58:32 -0500

    Debug 12

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 const port = process.env.PORT || 8080;
-const DB_HOST = process.env.DB_HOST || "mysql.railway.internal";
+const DB_HOST = process.env.DB_HOST || "junction.proxy.rlwy.net";
 const DB_USER = process.env.DB_USER || 'root';
-const DB_PASSWORD = process.env.DB_PASSWORD || "123456";
-const DB_NAME = process.env.DB_NAME || 'Ford_cotizaciones';
-const DB_PORT = process.env.DB_PORT || 3306;
+const DB_PASSWORD = process.env.DB_PASSWORD || "cvAYDGboeYEAlwrIgCqsxhAQvVnxUJHe";
+const DB_NAME = process.env.DB_NAME || 'railway';
+const DB_PORT = process.env.DB_PORT || 58483;
 module.exports = { port, DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT };
