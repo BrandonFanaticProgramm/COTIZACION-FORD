1df3f41e72cb78aede1b3230c9e5c390b3881200
BrandonFanaticProgramm <brandonalexisquintero.sena@gmail.com>
2024-12-14 14:33:32 -0500

    Debug 12

--- server/server.js
+++ server/server.js
@@ -15,11 +15,11 @@ let id_usuario = "";
 // Crear la conexión utilizando Promesas
 async function conexion() {
   const conexion = await mysql.createConnection({
-    user: 'root',
-    password: '123456',
-    host: 'localhost',
-    port: 3306,
-    database: 'Ford_cotizaciones',
+    user: DB_USER,
+    password: DB_PASSWORD,
+    host: DB_HOST,
+    port: DB_PORT,
+    database: DB_NAME,
   });
   return conexion;
 }
